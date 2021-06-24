017c1f8f610dc6cf8f183b3aa73e7d81081dfc63
David Armendariz <darmendariz1998@outlook.com>
2021-06-24 18:46:05 -0500

    Styling tabs

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,9 +1,13 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')"
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resource')"
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
       >Add Resource</base-button
     >
   </base-card>
@@ -21,12 +25,39 @@ export default {
   },
   data() {
     return {
-      selectedTab: 'stored-resources'
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'You need to learn to google.',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources
     };
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    }
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
     }
   }
 };
